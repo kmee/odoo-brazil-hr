0fba2759e7ddbccb30240888231af41bb23f3701
Rodolfo Bertozo <rodolfobertozo@gmail.com>
2015-04-23 09:18:13 -0300

    [NEW] Criei a classe hr_payslip e sobrescrevi o método compute_sheet [FIX] Corrigi o método _get_worked_days que não estava filtrando os ids corretamente

--- l10n_br_hr_payroll/model/hr_contract.py
+++ l10n_br_hr_payroll/model/hr_contract.py
@@ -79,14 +79,17 @@ class HrContract(orm.Model):
         res = {}
     
         obj_worked_days = self.pool.get('hr.payslip.worked_days')
-        worked_ids =  obj_worked_days.search(cr, uid, [('contract_id', '=', ids[0])])
+        payslip_id = context['payslip_id']
+        worked_ids = obj_worked_days.search(
+            cr, uid, [('contract_id', '=', ids[0]),
+                      ('payslip_id', '=', payslip_id),
+                      ('code', 'like', 'WORK100')])
         if worked_ids:
             worked = obj_worked_days.browse(cr, uid, worked_ids[0])
             res[ids[0]] = worked.number_of_days
-            return res
         else:
             res[ids[0]] = 0
-            return res
+        return res
     
     def _check_date(self, cr, uid, ids, fields, arg, context=None):
         res = {}
@@ -135,13 +138,3 @@ class HrContract(orm.Model):
         'value_va' : 0, 
         'value_vr' : 0  
     }
-
-
-    
-
-    
-     
-
-   
-    
-
